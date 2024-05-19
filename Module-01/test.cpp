4d2b0132be04881672c76bda684e75f4c5aa0cdc
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-05-19 17:54:24 +0600

    testing first c++ code

---
+++ Module-01/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << "a: " << a << endl;
+    int b = 5;
+    cout << "b: " << b << endl;
+    int result = a + b;
+    cout << "sum of a and b: " << result << endl;
+    return 0;
+}
