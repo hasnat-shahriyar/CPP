1193379624625725db80e7745fc1b3e6a5971d24
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-05-21 01:27:46 +0600

    string in c++

--- Module-01/string.cpp
+++ Module-01/string.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     char s[100];
     cin.getline(s, 100);
-    cout << s << endl;   
+    cout << s << endl;
     return 0;
 }
